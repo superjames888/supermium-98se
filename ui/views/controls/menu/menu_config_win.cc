8874ac599a7e5e5b96678d77448a9f0867d7539a
win32ss <win32ss@nulln.nl>
2024-08-31 17:04:54 -0400

    Revert forcing of Chrome Refresh 2023 on menus and... revert "Don't show the 'Search this page with...' item in incognito mode."
    This reverts commit 8785806a79a8e17e1f1c10c5452982a0673c394c.

--- ui/views/controls/menu/menu_config_win.cc
+++ ui/views/controls/menu/menu_config_win.cc
@@ -2,12 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/command_line.h"
 #include "ui/views/controls/menu/menu_config.h"
 
 #include <windows.h>  // Must come before other Windows system headers.
 
 #include <Vssym32.h>
 
+#include "ui/base/ui_base_features.h"
 #include "ui/gfx/system_fonts_win.h"
 
 namespace views {
@@ -15,7 +17,33 @@ namespace views {
 void MenuConfig::InitPlatform() {
   context_menu_font_list = font_list =
       gfx::FontList(gfx::win::GetSystemFont(gfx::win::SystemFont::kMenu));
-
+  bool is_refresh = features::IsChromeRefresh2023();
+  
+  if (!is_refresh) {
+    if (base::CommandLine::ForCurrentProcess()->HasSwitch("win11-menus")) {
+      corner_radius = 8;
+      menu_horizontal_border_size = 4;
+      submenu_horizontal_overlap = 1;
+      rounded_menu_vertical_border_size = 4;
+      item_horizontal_padding = 12;
+      between_item_vertical_padding = 2;
+      separator_height = 1;
+      separator_upper_height = 1;
+      separator_lower_height = 1;
+      item_corner_radius = 4;
+    } else {
+      menu_horizontal_border_size = 3;
+      nonrounded_menu_vertical_border_size = 3;
+      item_vertical_margin = 3;
+      item_horizontal_border_padding = -2;
+      icon_label_spacing = 10;
+      always_reserve_check_region = true;
+      separator_height = 7;
+      separator_upper_height = 5;
+      separator_lower_height = 5;
+    }
+  }
+  
   BOOL show_cues;
   show_mnemonics =
       (SystemParametersInfo(SPI_GETKEYBOARDCUES, 0, &show_cues, 0) &&
