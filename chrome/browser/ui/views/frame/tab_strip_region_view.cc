6ecacc18d9fb5898b4735e80a3c94d8f560f1e92
win32ss <win32ss@nulln.nl>
2024-09-04 16:24:12 -0400

    Revert "Clean up isChromeRefresh2023 for NTB positioning in TabStripRegionView."
    This reverts commit b11c7c1b8915bff7067e6a0a7a6fa762e25bcebd.

--- chrome/browser/ui/views/frame/tab_strip_region_view.cc
+++ chrome/browser/ui/views/frame/tab_strip_region_view.cc
@@ -92,7 +92,8 @@ bool ShouldShowNewTabButton(const Browser* browser) {
 
 TabStripRegionView::TabStripRegionView(std::unique_ptr<TabStrip> tab_strip)
     : render_tab_search_before_tab_strip_(
-          TabSearchBubbleHost::ShouldTabSearchRenderBeforeTabStrip()) {
+          TabSearchBubbleHost::ShouldTabSearchRenderBeforeTabStrip()),
+      render_new_tab_button_over_tab_strip_(features::IsChromeRefresh2023()) {
   views::SetCascadingColorProviderColor(
       this, views::kCascadingBackgroundColor,
       kColorTabBackgroundInactiveFrameInactive);
@@ -262,8 +263,9 @@ bool TabStripRegionView::IsRectInWindowCaption(const gfx::Rect& rect) {
   };
 
   // Perform checks for buttons that should be rendered above the tabstrip.
-  if (new_tab_button_ && new_tab_button_->GetLocalBounds().Intersects(
-                             get_target_rect(new_tab_button_))) {
+  if (render_new_tab_button_over_tab_strip_ && new_tab_button_ &&
+      new_tab_button_->GetLocalBounds().Intersects(
+          get_target_rect(new_tab_button_))) {
     return !new_tab_button_->HitTestRect(get_target_rect(new_tab_button_));
   }
 
@@ -380,7 +382,7 @@ void TabStripRegionView::Layout(PassKey) {
                          product_specifications_button_width);
   }
 
-  if (new_tab_button_) {
+  if (render_new_tab_button_over_tab_strip_ && new_tab_button_) {
     // The NTB needs to be layered on top of the tabstrip to achieve negative
     // margins.
     gfx::Size new_tab_button_size = new_tab_button_->GetPreferredSize();
@@ -494,7 +496,10 @@ void TabStripRegionView::UpdateButtonBorders() {
   // definitely isn't what we want in the scrolling case, so this naive approach
   // should be improved, likely by taking the scroll state of the tabstrip into
   // account.
-  const auto border_insets = gfx::Insets::TLBR(top_inset, 0, bottom_inset, 0);
+  constexpr int kHorizontalInset = 8;
+  const auto border_insets =
+      gfx::Insets::TLBR(top_inset, 0, bottom_inset,
+                        features::IsChromeRefresh2023() ? 0 : kHorizontalInset);
   if (new_tab_button_) {
     new_tab_button_->SetBorder(views::CreateEmptyBorder(border_insets));
   }
@@ -509,22 +514,23 @@ void TabStripRegionView::UpdateButtonBorders() {
 }
 
 void TabStripRegionView::UpdateTabStripMargin() {
-  // The new tab button overlaps the tabstrip. Render it to a layer and adjust
-  // the tabstrip right margin to reserve space for it.
+  //  If the new tab button or tab search button are positioned over the
+  //  tabstrip, then buttons are rendered to a layer, and the margins are set to
+  //  take up the rest of the space under the buttons.
   std::optional<int> tab_strip_right_margin;
   if (new_tab_button_) {
-    new_tab_button_->SetPaintToLayer();
-    new_tab_button_->layer()->SetFillsBoundsOpaquely(false);
-    // Inset between the tabstrip and new tab button should be reduced to
-    // account for extra spacing.
-    new_tab_button_->SetProperty(views::kViewIgnoredByLayoutKey, true);
+    if (render_new_tab_button_over_tab_strip_) {
+      new_tab_button_->SetPaintToLayer();
+      new_tab_button_->layer()->SetFillsBoundsOpaquely(false);
+      // Inset between the tabstrip and new tab button should be reduced to
+      // account for extra spacing.
+      new_tab_button_->SetProperty(views::kViewIgnoredByLayoutKey, true);
 
-    tab_strip_right_margin = new_tab_button_->GetPreferredSize().width() +
-                             GetLayoutConstant(TAB_STRIP_PADDING);
+      tab_strip_right_margin = new_tab_button_->GetPreferredSize().width() +
+                               GetLayoutConstant(TAB_STRIP_PADDING);
+    }
   }
 
-  // If the tab search button is before the tab strip, it also overlaps the
-  // tabstrip, so give it the same treatment.
   std::optional<int> tab_strip_left_margin;
   if (tab_search_container_ && render_tab_search_before_tab_strip_) {
     // The `tab_search_container_` is being laid out manually.
